14cd55325dde0b6cdfa3365f36d00dc141eafe08
xuxin666666 <3268154595@qq.com>
2022-01-14 17:01:23 +0800

    完成了首页功能的开发

--- src/main.js
+++ src/main.js
@@ -1,5 +1,26 @@
 import Vue from 'vue'
+import {$http} from '@escook/request-miniprogram'
+
 import App from './App'
+
+uni.$http = $http
+$http.baseUrl = 'https://www.uinav.com'
+$http.beforeRequest = function(options) {
+    uni.showLoading({
+        title: '数据加载中...'
+    })
+}
+$http.afterRequest = function(options) {
+    uni.hideLoading()
+}
+
+uni.$showMsg = function(title='数据请求失败！', duration=1500) {
+    uni.showToast({
+        title,
+        duration,
+        icon: 'none'
+    })
+}
 
 Vue.config.productionTip = false
 
--- src/pages/home/home.vue
+++ src/pages/home/home.vue
@@ -1,6 +1,34 @@
 <template>
     <view>
-        home 
+        <swiper indicator-dots autoplay :interval="3000" :duration="1000" circular>
+            <swiper-item v-for="(item, i) in swiperList" :key="i">
+                <navigator class="swiper-item" :url="'/subpkg/goods_detail/goods_detail?goods_id=' + item.goods_id">
+                    <image :src="item.image_src" />
+                </navigator>
+            </swiper-item>
+        </swiper>
+        <view class="nav-list">
+            <view class="nav-item" v-for="(item, i) in navList" :key="i" @click="navClickHandler(item)">
+                <image :src='item.image_src' class="nav-img" />
+            </view>
+        </view>
+        <view class="floor-list">
+            <view class="floor-item" v-for="(item, i) in floorList" :key="i">
+                <image :src="item.floor_title.image_src" class="floor-title" />
+                <view class="floor-img-box">
+                    <navigator class="left-box" :url="item.product_list[0].url">
+                        <image :src="item.product_list[0].image_src" :style="{width: item.product_list[0].image_width + 'rpx'}" mode="widthFix" />
+                    </navigator>
+                    <view class="right-box">
+                        <template v-for="(item2, i2) in item.product_list">
+                            <navigator v-if="i2 !== 0" :key="i2" :url="item2.url">
+                                <image :src="item2.image_src" :style="{width: item2.image_width + 'rpx'}" mode="widthFix"></image>
+                            </navigator>
+                        </template>
+                    </view>
+                </view>
+            </view>
+        </view>
     </view>
 </template>
 
@@ -8,11 +36,95 @@
 export default {
     data() {
         return {
+            swiperList: [],
+            navList: [],
+            floorList: []
+        };
+    },
+    // computed: {
+    //     rightImgs: function() {
+    //         return this.floorList.filter
+    //     }
+    // },
+    onLoad() {
+        this.getSwiperList();
+        this.getNavList()
+        this.getFloorList()
+    },
+    methods: {
+        async getSwiperList() {
+            const { data: res } = await uni.$http.get(
+                "/api/public/v1/home/swiperdata"
+            );
+            if (res.meta.status !== 200) 
+                return uni.$showMsg()
+            this.swiperList = res.message;
+        },
+        async getNavList() {
+            const {data: res} = await uni.$http.get('/api/public/v1/home/catitems')
+            if(res.meta.status !== 200)
+                return uni.$showMsg()
+            this.navList = res.message
+        },
+        navClickHandler(item) {
+            if(item.name === '分类') {
+                uni.switchTab({
+                    url: '/pages/cate/cate'
+                })
+            }
+        },
+        async getFloorList() {
+            const {data: res} = await uni.$http.get('/api/public/v1/home/floordata')
+            if(res.meta.status !== 200)
+                return uni.$showMsg()
 
+            res.message.forEach(floor => {
+                floor.product_list.forEach(prod => {
+                    prod.url = '/subpkg/goods_list/goods_list?' + prod.navigator_url.split('?')[1]
+                })
+            })
+            this.floorList = res.message
         }
-    }
+    },
 };
 </script>
 
-<style>
+<style lang='scss'>
+swiper {
+    height: 330upx;
+    .swiper-item,
+    image {
+        width: 100%;
+        height: 100%;
+    }
+}
+.nav-list {
+    display: flex;
+    justify-content: space-around;
+    margin: 30upx 0;
+    .nav-img {
+        width: 128upx;
+        height: 140upx;
+    }
+}
+.floor-list{
+    .floor-title {
+        width: 100%;
+        height: 60upx;
+    }
+    .floor-img-box {
+        display: flex;
+        padding-left: 10upx;
+        .right-img-item {
+            width: 0;
+            /* height: 0; */
+        }
+        .right-box {
+            display: flex;
+            flex-wrap: wrap;
+            justify-content: space-around;
+        }
+    }
+    
+}
 </style>
